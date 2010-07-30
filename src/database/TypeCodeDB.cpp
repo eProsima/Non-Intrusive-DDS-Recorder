c5202c71b1c25cfc0198aa0bfdcd627acc93f103
Ricardo González Moreno <RicardoGonzalez@eprosima.com>
2010-07-30 11:18:40 +0000

    Update before use only database

--- src/database/TypeCodeDB.cpp
+++ src/database/TypeCodeDB.cpp
@@ -4,6 +4,8 @@
 #include "cdr/cdr_stream.h"
 #include "osapi/osapi_heap.h"
 
+#define TYPECODE_TABLE "typecodes"
+
 using namespace eProsima;
 using namespace std;
 
@@ -41,6 +43,49 @@ bool eTypeCode::equal(const char *topicName, const char *typeName)
 
 TypeCodeDB::TypeCodeDB(eProsimaLog &log, sqlite3 *databaseH) : m_log(log), m_databaseH(databaseH)
 {
+    const char* const METHOD_NAME = "TypeCodeDB";
+    const char* const TABLE_CHECK = "SELECT name FROM sqlite_master WHERE name=\"" TYPECODE_TABLE "\"";
+    const char* const TABLE_TRUNCATE = "DELETE FROM " TYPECODE_TABLE;
+    const char* const TABLE_CREATE = "CREATE TABLE " TYPECODE_TABLE "(topic_name VARCHAR(50)," \
+                                      "type_name VARCHAR(50), typecode VARCHAR(2048)," \
+                                      "PRIMARY KEY(topic_name, type_name))";
+    sqlite3_stmt *stmt = NULL;
+    int ret = SQLITE_ERROR;
+
+    if(sqlite3_prepare_v2(m_databaseH, TABLE_CHECK, strlen(TABLE_CHECK), &stmt, NULL) == SQLITE_OK)
+    {
+        ret = sqlite3_step(stmt);
+        sqlite3_finalize(stmt);
+
+        if(ret == SQLITE_ROW)
+        {
+            if(sqlite3_prepare_v2(m_databaseH, TABLE_TRUNCATE, strlen(TABLE_TRUNCATE), &stmt, NULL) == SQLITE_OK)
+            {
+                if(sqlite3_step(stmt) != SQLITE_DONE)
+                    logError(m_log, "Cannot truncate the typecodes table");
+
+                sqlite3_finalize(stmt);
+            }
+        }
+        else if(ret == SQLITE_DONE)
+        {
+            if(sqlite3_prepare_v2(m_databaseH, TABLE_CREATE, strlen(TABLE_CREATE), &stmt, NULL) == SQLITE_OK)
+            {
+                if(sqlite3_step(stmt) != SQLITE_DONE)
+                    logError(m_log, "Cannot create the typecodes table");
+
+                sqlite3_finalize(stmt);
+            }
+        }
+        else
+        {
+            logError(m_log, "Cannot execute check the typecodes table");
+        }
+    }
+    else
+    {
+        logError(m_log, "Cannot check the typecodes table");
+    }
 }
 
 TypeCodeDB::~TypeCodeDB()
