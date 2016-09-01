ada931cee32fe83c0b64ca94e635e1ce22da3743
Santiago Munoz <santiagomunoz@eprosima.com>
2016-09-01 12:33:54 +0200

    Properly add fastrtps and fastcdr submodules, add functions to find pcap and sqlite3

---
+++ cmake/dev/external_dependencies.cmake
@@ -0,0 +1,44 @@
+macro (find_pcap)
+    find_path( PCAP_INCLUDE_DIR
+        NAMES
+        pcap.h
+        PATHS
+            "/usr/include/"
+    )
+    find_library( PCAP_LIBRARY
+        NAMES
+        pcap
+        wpcap
+        PATHS
+            "/usr/lib/"
+    )
+    if(NOT PCAP_INCLUDE_DIR)
+        set(PCAP_FOUND FALSE)
+        message(STATUS "pcap not found!")
+    else()
+        set(PCAP_FOUND TRUE)
+        message(STATUS "Found pcap!")
+    endif()
+endmacro(find_pcap)
+
+macro (find_sqlite3)
+    find_path( SQLITE3_INCLUDE_DIR
+        NAMES
+        sqlite3.h
+        PATHS
+            "/usr/include/"
+    )
+    find_library( SQLITE3_LIBRARY
+        NAMES
+        libsqlite3
+        PATHS
+            "/usr/lib/"
+    )
+    if(NOT SQLITE3_INCLUDE_DIR)
+        set(SQLITE3_FOUND FALSE)
+        message(STATUS "sqlite3 not found!")
+    else()
+        set(SQLITE3_FOUND TRUE)
+        message(STATUS "sqlite3 found!")
+    endif()
+endmacro(find_sqlite3)
