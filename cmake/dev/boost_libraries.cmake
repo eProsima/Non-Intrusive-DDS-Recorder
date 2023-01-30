07a0b5acbe60ac698c747b6bc5b7b338494e8b7a
Ricardo González <correoricky@gmail.com>
2023-01-30 14:26:07 +0100

    Refs #16931. Update installer mechanism
    Signed-off-by: Ricardo González <correoricky@gmail.com>

--- cmake/dev/boost_libraries.cmake
+++ cmake/dev/boost_libraries.cmake
@@ -26,80 +26,15 @@ macro(check_boost)
     endif()
 endmacro()
 
-macro(install_boost FILETYPE)
+macro(install_boost)
     if(MSVC OR MSVC_IDE)
-        set(RUNTIME_FILES_ 0)
-        set(LIBRARY_FILES_ 0)
-        if("${FILETYPE}" MATCHES "^([Rr][Uu][Nn][Tt][Ii][Mm][Ee])$")
-            set(RUNTIME_FILES_ 1)
-        elseif("${FILETYPE}" MATCHES "^([Ll][Ii][Bb][Rr][Aa][Rr][Yy])$")
-            set(LIBRARY_FILES_ 1)
-        elseif("${FILETYPE}" MATCHES "^([Aa][Ll][Ll])$")
-            set(RUNTIME_FILES_ 1)
-            set(LIBRARY_FILES_ 1)
-        else()
-            message(FATAL_ERROR "Bad parameter in install_boost macro")
-        endif()
-
         foreach(arg_ ${ARGN})
             if(EPROSIMA_BUILD)
-                if(MSVC10)
-                    set(BOOST_ARCH "vc100")
-                elseif(MSVC11)
-                    set(BOOST_ARCH "vc110")
-                elseif(MSVC12)
-                    set(BOOST_ARCH "vc120")
-                elseif(MSVC14)
-                    set(BOOST_ARCH "vc140")
-                endif()
-
-                set(DIR_EXTENSION "")
-                if(EPROSIMA_INSTALLER_MINION)
-                    set(DIR_EXTENSION "/${MSVC_ARCH}")
-                endif()
-
-                #Normalize path
-                get_filename_component(BOOST_LIBRARYDIR_NORMALIZE ${BOOST_LIBRARYDIR} ABSOLUTE)
-
-                # Runtime
-                if(RUNTIME_FILES_)
-                    install(DIRECTORY ${BOOST_LIBRARYDIR_NORMALIZE}/
-                        DESTINATION ${BIN_INSTALL_DIR}${DIR_EXTENSION}
-                        COMPONENT binaries
-                        CONFIGURATIONS Debug
-                        FILES_MATCHING
-                        PATTERN "boost_${arg_}-${BOOST_ARCH}-mt-gd*.dll"
-                        )
-
-                    install(DIRECTORY ${BOOST_LIBRARYDIR_NORMALIZE}/
-                        DESTINATION ${BIN_INSTALL_DIR}${DIR_EXTENSION}
-                        COMPONENT binaries
-                        CONFIGURATIONS Release
-                        FILES_MATCHING
-                        PATTERN "boost_${arg_}-${BOOST_ARCH}-mt*.dll"
-                        PATTERN "boost_${arg_}-${BOOST_ARCH}-mt-gd*.dll" EXCLUDE
-                        )
-                endif()
-
-                # Library
-                if(LIBRARY_FILES_)
-                    install(DIRECTORY ${BOOST_LIBRARYDIR_NORMALIZE}/
-                        DESTINATION ${LIB_INSTALL_DIR}${DIR_EXTENSION}
-                        COMPONENT libraries
-                        CONFIGURATIONS Debug
-                        FILES_MATCHING
-                        PATTERN "boost_${arg_}-${BOOST_ARCH}-mt-gd*.lib"
-                        )
-
-                    install(DIRECTORY ${BOOST_LIBRARYDIR_NORMALIZE}/
-                        DESTINATION ${LIB_INSTALL_DIR}${DIR_EXTENSION}
-                        COMPONENT libraries
-                        CONFIGURATIONS Release
-                        FILES_MATCHING
-                        PATTERN "boost_${arg_}-${BOOST_ARCH}-mt*.lib"
-                        PATTERN "boost_${arg_}-${BOOST_ARCH}-mt-gd*.lib" EXCLUDE
-                        )
-                endif()
+                install(FILES ${boost_lib_release}
+                    DESTINATION ${BIN_INSTALL_DIR}
+                    COMPONENT binaries
+                    CONFIGURATIONS Release
+                    )
             endif()
         endforeach()
     endif()
